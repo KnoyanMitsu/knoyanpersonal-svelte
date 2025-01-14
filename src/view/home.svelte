4640c7917f0525b05b9736d79a2ea250223d631d
Knoyan Mitsu <yanappuowo@yandex.com>
2025-01-14 19:19:11 +0700

    Delete Unused import

--- src/view/home.svelte
+++ src/view/home.svelte
@@ -1,15 +1,13 @@
 <script>
-  import Title from "../view/title.svelte";
-  import About from '../view/about.svelte';
-  import { Icon } from 'svelte-icons-pack';
-  import { IoArrowDownSharp } from 'svelte-icons-pack/io';
-  import ProjectView from '../view/project.svelte';
-  import ContactView from '../view/contact.svelte';
+	import Title from '../view/title.svelte';
+	import About from '../view/about.svelte';
+	import ProjectView from '../view/project.svelte';
+	import ContactView from '../view/contact.svelte';
 </script>
 
 <div class="lg:overflow-y-scroll lg:snap-y lg:snap-mandatory h-screen lg:w-screen">
 	<div class="container mx-auto">
-    <Title />  
+		<Title />
 		<div class="lg:snap-start lg:h-screen justify-center items-center flex mb-8">
 			<About />
 		</div>
